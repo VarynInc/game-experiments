7a38bb021a632d7a1a8d06b05ada3049913b74e1
John Foster <varyndev@gmail.com>
2021-08-23 15:55:40 -0700

    update rundev task

--- path-animate/bin/rundev.js
+++ path-animate/bin/rundev.js
@@ -2,10 +2,27 @@
  * Run the development server. This will serve the project directly from the source folder with a static
  * HTTP server.
  */
-var localPort = 8000;
-var express = require("express");
-var serveStatic = require("serve-static");
-var webserver = express();
-webserver.use(serveStatic("./", {"index": ["demo.html"]}));
-webserver.listen(localPort);
-console.log("Listening on port " + localPort + " open a browser to http://localhost:" + localPort);
+ const express = require("express");
+ const serveStatic = require("serve-static");
+ const webserver = express();
+ let localPort = 8000;
+ webserver.use(serveStatic("./", {"index": ["demo.html"]}));
+ 
+ const server = webserver.listen(localPort);
+ 
+ server.on('error', function(serverError) {
+     console.log('Trying port ' + localPort + '...');
+     if (serverError.code === 'EADDRINUSE') {
+       console.log('Port ' + localPort + ' is in use, retrying with a different port...');
+       localPort += 1;
+       setTimeout(function() {
+         server.close();
+         console.log('Trying port ' + localPort + '...');
+         server.listen(localPort);
+       }, 2000);
+     }
+ });
+ server.on('listening', function() {
+   console.log("Listening on port " + localPort + ": open a browser to http://localhost:" + localPort);
+ });
+ 
